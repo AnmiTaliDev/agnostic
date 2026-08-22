d7b758c9f28cf30f575b2b1dcd657b4afbd481b2
AnmiTaliDev <anmitali198@gmail.com>
2026-08-22 16:35:53 +0500

    feat: self-host the lexer (src/lexer/lexer.agn)

---
+++ src/lexer/agn_embed_shim.cpp
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2026 AnmiTaliDev <anmitalidev@nuros.org>
+
+extern "C" long agn_argc = 0;
+extern "C" const char** agn_argv = nullptr;
